66ada7f8f699dffc56452b1f2c1c95d57ff47d3b
toolbakery <toolbakery@users.noreply.github.com>
2016-03-10 16:18:52 +0100

    code format, support for poster categories, some functions extracted, detect already applied modifications

--- gaf-visual-posts/gaf-visual-posts.js
+++ gaf-visual-posts/gaf-visual-posts.js
@@ -4,7 +4,7 @@
 // @description NeoGAF visual posts
 // @include     http://*.neogaf.com/forum/showthread.php?*
 // @version     1
-// @run-at	document-start|document-end
+// @run-at      document-start|document-end
 // @grant       none
 // ==/UserScript==
 
@@ -18,56 +18,67 @@
 */
 
 
-var categories = [
-    {
-    	name : 'anime',
-    	keywords : ['anime', 'jojo', 'naruto', 'gintama'],
-    	color : 'blue',
-    },
-    {
-    	name : 'wrestling',
-    	keywords : ['wrestling', 'hogan', 'cena', 'new day', 'xavier', 'McMahon'],
-    	color : 'green',
-    },
-    {
-    	name : 'Max Payne',
-    	keywords : ['payne', 'MP3'],
-    	color : 'red',
+var categories = {
+    posts: [{
+        name: 'anime',
+        keywords: ['anime', 'jojo', 'naruto', 'gintama'],
+        color: 'blue',
+    }, {
+        name: 'wrestling',
+        keywords: ['wrestling', 'hogan', 'cena', 'new day', 'xavier', 'McMahon'],
+        color: 'green',
+    }, {
+        name: 'Max Payne',
+        keywords: ['payne', 'MP3'],
+        color: 'red',
+    }, ],
+    poster: [{
+        name: 'GB Staff',
+        keywords: ['rudds', 'bionicpuppy', 'wreckthelaw', 'monsterdunk', 'danryckert'],
+        color: 'yellow',
+    }, ]
+};
+
+function modifyElement(elem, category) {
+    descriptiveText = "[" + category.name.toUpperCase() + "]";
+    var catNameSpan;
+    gvpElements = elem.getElementsByClassName("gvp");
+    if (gvpElements.length > 0) {
+        // span already exists
+        catNameSpan = gvpElements[0];
+    } else {
+        catNameSpan = document.createElement("span");
+        catNameSpan.classList.add("gvp");
+        elem.insertBefore(catNameSpan, elem.firstChild);
     }
-];
-
-/**
- * unused function
- */
-function getKeywordDict(cats){
-	var dict = {};
-	for each (category in cats) {
-		for each (keyword in category.keywords) {
-			dict[keyword] = category.color;
-		}
-	}
-	return dict;
+    // rather add category name as class and define global css with the desired properties
+    elem.style.backgroundColor = category.color;
+    if (String(catNameSpan.innerHTML).indexOf(descriptiveText) == -1) {
+        catNameSpan.appendChild(document.createTextNode(descriptiveText));
+    }
 }
 
-function highlight_categories(){
-	for each (elem in document.getElementsByClassName('post')) {
-		var post = String(elem.innerHTML).toLowerCase();
-		for each (category in self.categories) {
-			var modifiedStyleValue = "background-color:"+ category.color + ";";
-			for each (keyword in category.keywords) {
-				if (post.indexOf(keyword.toLowerCase()) > -1) {
-					// rather add category name as class and define global css with the desired properties
-					elem.style.backgroundColor = category.color;
-					var catNameSpan = document.createElement("span");
-					catNameSpan.appendChild(document.createTextNode("[" + category.name.toUpperCase() + "] "));
-					elem.insertBefore(catNameSpan, elem.firstChild);
-					break;
-				}
-			}
-		}
-	};
+function highlight(elements, definitions) {
+    for each(elem in elements) {
+        var textToAnalyze = String(elem.innerHTML).toLowerCase();
+        for each(category in definitions) {
+            for each(keyword in category.keywords) {
+                if (textToAnalyze.indexOf(keyword.toLowerCase()) > -1) {
+                    modifyElement(elem, category);
+                    // one keyword matched, no need to check others
+                    break;
+                }
+            }
+        }
+    };
+
 }
 
-(function() {	
-	highlight_categories();
+function highlight_categories() {
+    highlight(document.getElementsByClassName('postbit-details-username'), self.categories.poster);
+    highlight(document.getElementsByClassName('post'), self.categories.posts);
+}
+
+(function() {
+    highlight_categories();
 })();
